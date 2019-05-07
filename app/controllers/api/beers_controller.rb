328966f8434d2c204bda5663774f3dbb0d8f7091
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-07 17:53:17 -0600

    build #create, #show and pricate#beer_params

--- app/controllers/api/beers_controller.rb
+++ app/controllers/api/beers_controller.rb
@@ -2,4 +2,22 @@ class Api::BeersController < ApplicationController
   def index
     render json: Beer.all
   end
+
+  def create
+    beer = Beer.new(beer_params)
+    if beer.save
+      render json: beer
+    else
+      render json: { message: beer.errors }, status: 400
+    end
+  end
+
+  def show
+    render json: Beer.find_by(id: params[:id])
+  end
+
+  private
+    def beer_params
+      params.require(:beer).permit(:name, :brand, :style, :abv)
+    end
 end
